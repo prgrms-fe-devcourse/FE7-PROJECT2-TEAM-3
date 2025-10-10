b0ecf3c9ff119bab29ab1b535792dc80e57b29f6
stupilman <creworroad@naver.com>
2025-10-10 16:08:42 +0900

    ✨ supabase 기본 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import PostCreatePage from "./pages/posts/PostCreatePage";
+
 export default function App() {
   return (
     <>
-      <h1>App Component</h1>
+      {/* <h1>App Component</h1> */}
+      <PostCreatePage />
     </>
   );
 }
