06f414bd18a1848decdeba571c8146e229b9fb63
stupilman <creworroad@naver.com>
2025-10-13 14:47:48 +0900

    🚧 PostCreatePage 실제 user로 변경 예정 및 PostList 구현 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import PostCreatePage from "./pages/posts/PostCreatePage";
+// import PostCreatePage from "./pages/posts/PostCreatePage";
+import PostsList from "./pages/posts/PostsList";
 
 export default function App() {
   return (
     <>
       {/* <h1>App Component</h1> */}
-      <PostCreatePage />
+      <PostsList />
     </>
   );
 }
