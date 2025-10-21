d3819322feecf9807eed9f1a850268bca7846b3b
suyeon <suyeon1104@gmail.com>
2025-10-21 21:58:01 +0900

    댓글 줄 띄어쓰기 표시

--- src/pages/posts/Comment.tsx
+++ src/pages/posts/Comment.tsx
@@ -114,7 +114,7 @@ const Comment = ({
             rows={4}
           />
         ) : (
-          <p className="text-sm text-gray-300 leading-relaxed">{content}</p>
+          <p className="text-sm text-gray-300 leading-relaxed whitespace-pre-line">{content}</p>
         )}
       </div>
     </div>
