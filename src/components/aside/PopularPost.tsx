64043f146892448b758a6bbb47fc13674dbb7520
ivoryShield <sangapark34@naver.com>
2025-10-21 15:49:59 +0900

    ✅ 인기게시물 클릭시 링크 연결

--- src/components/aside/PopularPost.tsx
+++ src/components/aside/PopularPost.tsx
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 import type { PopularPosts } from "../../types/post";
 import ProfileImage from "../ui/ProfileImage";
 import { formaRelativeTime } from "../../utils/formatRelativeTime";
+import { Link } from "react-router";
 
 export default function PopularPost({
   post,
@@ -13,36 +14,38 @@ export default function PopularPost({
 }) {
   const createdText = formaRelativeTime(post.created_at);
   return (
-    <article className="flex gap-3 p-3 bg-[#161C27] rounded-lg cursor-pointer hover:opacity-70">
-      <div className="overflow-hidden w-8 h-8 rounded-full">
-        <ProfileImage
-          className="w-8 h-8"
-          src={post.profiles.profile_image}
-          alt={post.profiles.display_name}
-        />
-      </div>
-      <div className="flex flex-col flex-1 gap-2">
-        <div className="flex items-center flex-wrap gap-x-2.5">
-          <p className="text-sm text-white">{post.profiles.display_name}</p>
-          <p className="text-xs text-gray-400">{createdText}</p>
+    <article>
+      <Link to={`/posts/${post._id}`} className="flex gap-3 p-3 bg-[#161C27] rounded-lg cursor-pointer hover:opacity-70">
+        <div className="overflow-hidden w-8 h-8 rounded-full">
+          <ProfileImage
+            className="w-8 h-8"
+            src={post.profiles.profile_image}
+            alt={post.profiles.display_name}
+          />
         </div>
-        <h3 className="font-normal text-sm text-gray-300">{post.title}</h3>
-        <div className="flex gap-3">
-          <p className="flex-center gap-1 text-gray-400 text-xs">
-            <Heart
-              className={twMerge(
-                "w-3 h-3 stroke-gray-400 fill-gray-400",
-                liked && "stroke-red-600 fill-red-600"
-              )}
-            />
-            {post.likes_count}
-          </p>
-          <p className="flex-center gap-1 text-gray-400 text-xs">
-            <MessageSquare className="w-3 h-3 stroke-gray-400 fill-gray-400" />
-            {post.comments_count}
-          </p>
+        <div className="flex flex-col flex-1 gap-2">
+          <div className="flex items-center flex-wrap gap-x-2.5">
+            <p className="text-sm text-white">{post.profiles.display_name}</p>
+            <p className="text-xs text-gray-400">{createdText}</p>
+          </div>
+          <h3 className="font-normal text-sm text-gray-300">{post.title}</h3>
+          <div className="flex gap-3">
+            <p className="flex-center gap-1 text-gray-400 text-xs">
+              <Heart
+                className={twMerge(
+                  "w-3 h-3 stroke-gray-400 fill-gray-400",
+                  liked && "stroke-red-600 fill-red-600"
+                )}
+              />
+              {post.likes_count}
+            </p>
+            <p className="flex-center gap-1 text-gray-400 text-xs">
+              <MessageSquare className="w-3 h-3 stroke-gray-400 fill-gray-400" />
+              {post.comments_count}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </article>
   );
 }
